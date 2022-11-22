f87de16ca83ad1c7e71fd6b5d298165605dbaf06
Jamie <jamieh905@gmail.com>
2022-11-22 11:07:09 -0500

    Added selection page and features

--- B07ProjectApp/app/src/main/java/com/example/b07projectapp/TestFragment.java
+++ B07ProjectApp/app/src/main/java/com/example/b07projectapp/TestFragment.java
@@ -23,7 +23,7 @@ public class TestFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_test, container, false);
+        return inflater.inflate(R.layout.fragment_login_selection, container, false);
     }
 
     @Override
