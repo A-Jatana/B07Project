92ccda3915f2ab31bf7d5cd3fd1de7800d0a4dfe
Amitoz <amitoz2003@hotmail.com>
2022-11-23 23:05:50 -0500

    this is the student decision page

---
+++ B07ProjectApp/app/src/main/java/com/example/b07projectapp/StudentDecision.java
@@ -0,0 +1,47 @@
+package com.example.b07projectapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.b07projectapp.databinding.FragmentStudentDesicionBinding;
+
+public class StudentDecision extends Fragment {
+
+    private FragmentStudentDesicionBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentStudentDesicionBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(com.example.b07projectapp.StudentDecision.this)
+                        .navigate(R.id.action_studentDecision_to_studentLogin);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
