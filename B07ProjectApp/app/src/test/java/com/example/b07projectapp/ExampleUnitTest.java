8bd6f54b25fc2706a8912e6b29b99add6cd3e1fa
Jamie <jamieh905@gmail.com>
2022-12-05 20:07:06 -0500

    Started Mockito tests

--- B07ProjectApp/app/src/test/java/com/example/b07projectapp/ExampleUnitTest.java
+++ B07ProjectApp/app/src/test/java/com/example/b07projectapp/ExampleUnitTest.java
@@ -1,17 +1,34 @@
 package com.example.b07projectapp;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(MockitoJUnitRunner.class)
 public class ExampleUnitTest {
+    @Mock
+    Control.View view;
+
+    @Mock
+    Control.Model model;
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    //
+    public void test1 (){
+        when (view.getUsername()).thenReturn("");
+        when (view.getPassword()).thenReturn("");
+
+        Presenter p = new Presenter (model, view);
+        p.checkLogin();
+        verify(view).displayMessage("Fields cannot be empty!");
     }
 }
